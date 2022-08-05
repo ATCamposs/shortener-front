8bfea6ebe606569fb7441b5dbf631024544e2a3a
Andre Campos <andre.tatibano@gmail.com>
2022-08-05 08:43:33 -0300

    create atom for form error

---
+++ components/atom/AtomFormError.vue
@@ -0,0 +1,16 @@
+<template>
+  <p v-if="active" class="text-red-500 text-xs italic">
+    Please fill out this field.
+  </p>
+</template>
+<script>
+export default {
+  name: 'AtomFormError',
+  props: {
+    active: {
+      type: Boolean,
+      default: false
+    }
+  }
+}
+</script>
