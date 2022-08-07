46aed2948fbec3b3e862e6a2d6bb95167df21225
Andre Campos <andre.tatibano@gmail.com>
2022-08-07 07:57:03 -0300

    add validations and post request for register

--- components/molecule/MoleculeRegisterForm.vue
+++ components/molecule/MoleculeRegisterForm.vue
@@ -28,6 +28,7 @@
       <AtomLabel name="password" :description="$t('form.fields.password')" />
       <AtomInput
         :input-error="validationErros.password.length > 0"
+        type="password"
         @changeInputValue="user.password = $event"
       />
       <AtomFormError :messages="validationErros.password" />
@@ -37,6 +38,7 @@
       <AtomLabel name="repeat-password" :description="$t('form.fields.repeatPassword')" />
       <AtomInput
         :input-error="validationErros.repeatPassword.length > 0"
+        type="password"
         @changeInputValue="user.repeatPassword = $event"
       />
       <AtomFormError :messages="validationErros.repeatPassword" />
@@ -79,8 +81,11 @@ export default {
     }
   },
   methods: {
-    onSubmit () {
+    async onSubmit () {
       this.validateInputs()
+      if (this.checkIsValid()) {
+        await $fetch.raw('http://localhost:8080/api/auth/sign_up', { method: 'POST', body: JSON.stringify(this.user) })
+      }
     },
     validateInputs () {
       this.clearInputErrors()
@@ -91,14 +96,22 @@ export default {
       if (!/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(this.user.email)) {
         this.validationErros.email.push(this.$t('form.errors.mustBeAValidEmail'))
       }
-      if (!/(?=[#$-/:-?{-~!"^_`[\]a-zA-Z]*([0-9#$-/:-?{-~!"^_`[\]]))(?=[#$-/:-?{-~!"^_`[\]a-zA-Z0-9]*[a-zA-Z])[#$-/:-?{-~!"^_`[\]a-zA-Z0-9]{6,20}/g.test(this.validationErros.password)) {
-        this.validationErros.password.push(this.$t('form.errors.mustBeAlphaNumeric'))
+      if (!/^[a-zA-Z0-9\-_]{8,20}$/g.test(this.user.password)) {
         this.validationErros.password.push(this.$t('form.errors.onlyAlphanumeric'))
-        this.validationErros.password.push(this.$t('form.errors.onlyCharactersRange', { min: '6', max: '20' }))
+        this.validationErros.password.push(this.$t('form.errors.onlyCharactersRange', { min: '8', max: '20' }))
       }
       if (this.user.password !== this.user.repeatPassword) {
         this.validationErros.repeatPassword.push(this.$t('form.errors.mustBeLikePassword'))
       }
+    },
+    checkIsValid () {
+      let isValid = true
+      Object.entries(this.validationErros).forEach((element) => {
+        if (Object.keys(element[1]).length > 0) {
+          isValid = false
+        }
+      })
+      return isValid
     },
     clearInputErrors () {
       this.validationErros.username = []
