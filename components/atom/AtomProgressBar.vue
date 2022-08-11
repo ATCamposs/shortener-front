cf5e58ae748a091c54f89e5e0cad311312707273
Andre Campos <andre.tatibano@gmail.com>
2022-08-10 22:51:33 -0300

    create basic progressbar

---
+++ components/atom/AtomProgressBar.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-400">
+    <p class="bg-blue-600 h-2.5 rounded-full" :style="'width: '+ progress + '%'" />
+  </div>
+</template>
+<script setup lang="ts">
+interface ProgressBarProps {
+  progress: number
+}
+withDefaults(defineProps<ProgressBarProps>(), {
+  progress: 0
+})
+</script>
