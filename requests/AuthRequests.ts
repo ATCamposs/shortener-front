c7334fc14257ccc7a87969c38a8c7775e5562c67
Andre Campos <andre.tatibano@gmail.com>
2022-08-10 22:18:07 -0300

    update return from authrequest

--- requests/AuthRequests.ts
+++ requests/AuthRequests.ts
@@ -2,11 +2,10 @@ import { User } from '@/models/User'
 import { UserAlreadyExistsByEmailException } from '@/requests/errors/AuthErrors'
 import { UserRegisterRequest } from '@/types/Register'
 
-export async function register (userParams: UserRegisterRequest) : Promise<User> {
+export async function sendRegisterUserRequest (userParams: UserRegisterRequest) : Promise<User> {
   try {
     const res = await useCustomFetch('auth/sign_up', 'POST', userParams)
-    const user = Object.assign(new User(), res._data)
-    return user
+    return Object.assign(new User(), res._data)
   } catch (err) {
     if (err.response._data.message === 'Email is already taken!') {
       throw new UserAlreadyExistsByEmailException('emailAlreadyInUse')
