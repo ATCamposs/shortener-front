d7093603f0a26afe2fe8ae7f8bcaadaf642e1cbf
Andre Campos <andre.tatibano@gmail.com>
2022-08-10 23:02:03 -0300

    update register molecule

--- components/molecule/MoleculeRegisterLoading.vue
+++ components/molecule/MoleculeRegisterLoading.vue
@@ -26,16 +26,16 @@ onMounted(() => {
       }
       progress.value++
     }
-  }, 100)
+  }, 50)
 })
 </script>
 
 <style scoped>
 .slide-fade-enter-active {
-  transition: all .3s ease;
+  transition: all .1s ease;
 }
 .slide-fade-leave-active {
-  transition: all .8s cubic-bezier(1.0, 0.5, 0.8, 1.0);
+  transition: all .3s cubic-bezier(1.0, 0.5, 0.8, 1.0);
 }
 .slide-fade-enter, .slide-fade-leave-to
 /* .slide-fade-leave-active for <2.1.8 */ {
