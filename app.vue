35a217a8c7120a3b23b814823056538671426046
Andre Campos <andre.tatibano@gmail.com>
2022-08-12 13:46:14 -0300

    add firebase

--- app.vue
+++ app.vue
@@ -1,5 +1,9 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div class="flex justify-center h-screen">
+    <MoleculeRegisterForm class="self-center w-1/4" />
+    <MoleculeLoginForm class="self-center w-1/4 ml-2" />
   </div>
 </template>
+<script setup>
+import '@/assets/css/tailwind.css'
+</script>
