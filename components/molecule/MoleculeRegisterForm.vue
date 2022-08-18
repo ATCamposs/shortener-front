36d5aec6a129d66478991591656f1395a871265a
Andre Campos <andre.tatibano@gmail.com>
2022-08-17 22:06:36 -0300

    update register form for firebase

--- components/molecule/MoleculeRegisterForm.vue
+++ components/molecule/MoleculeRegisterForm.vue
@@ -6,37 +6,20 @@
     <AtomError class="mb-5" :errors="validationErros.afterRequest" @closeRegisterErrorAlerts="closeErrorAlerts()" />
 
     <div class="mb-2">
-      <AtomLabel name="username" :description="$t('form.fields.username')" />
-      <AtomInput :value="user.username" :input-error="validationErros.username.length > 0" @changeInputValue="user.username = $event" />
-      <AtomFormError :messages="validationErros.username" />
-    </div>
-
-    <div class="mb-2">
       <AtomLabel name="email" :description="$t('form.fields.email')" />
-      <AtomInput :value="user.email" :input-error="validationErros.email.length > 0" @changeInputValue="user.email = $event" />
+      <AtomInput :value="userRegisterParams.email" :input-error="validationErros.email.length > 0" @changeInputValue="userRegisterParams.email = $event" />
       <AtomFormError :messages="validationErros.email" />
     </div>
 
     <div class="mb-2">
       <AtomLabel name="password" :description="$t('form.fields.password')" />
       <AtomInput
-        :value="user.password"
+        :value="userRegisterParams.password"
         :input-error="validationErros.password.length > 0"
         type="password"
-        @changeInputValue="user.password = $event"
+        @changeInputValue="userRegisterParams.password = $event"
       />
       <AtomFormError :messages="validationErros.password" />
-    </div>
-
-    <div class="mb-2">
-      <AtomLabel name="repeat-password" :description="$t('form.fields.repeatPassword')" />
-      <AtomInput
-        :value="user.repeatPassword"
-        :input-error="validationErros.repeatPassword.length > 0"
-        type="password"
-        @changeInputValue="user.repeatPassword = $event"
-      />
-      <AtomFormError :messages="validationErros.repeatPassword" />
     </div>
 
     <AtomButton class="mt-5" url="#" :name="$t('register')" />
@@ -47,7 +30,7 @@
 </template>
 <script setup lang="ts">
 import { useI18n } from 'vue-i18n'
-import { sendRegisterUserRequest } from '@/requests/AuthRequests'
+import { sendFormRegisterUserRequest } from '@/requests/AuthRequests'
 import { isInvalidEmail, isInvalidPassword } from '@/utils/Validations'
 const { t } = useI18n()
 
@@ -61,7 +44,7 @@ withDefaults(defineProps<MoleculeRegisterFormProps>(), {
   description: 'defaultDescription'
 })
 
-const user = useUserRegisterRequestParams()
+const userRegisterParams = useUserRegisterRequestParams()
 const validationErros = useUserRegisterValidationErrorsParams()
 const isSubmitting = useIsSubmittingRegister()
 
@@ -79,9 +62,9 @@ const onSubmit = () => {
   validateInputs()
   if (checkIsValid()) {
     isSubmitting.value = true
-    sendRegisterUserRequest(user.value)
+    sendFormRegisterUserRequest(userRegisterParams.value)
       .then((user) => {
-        setUserOnStorage(user)
+        setUserOnCookie(user)
         isSubmitting.value = false
       })
       .catch((err) => {
@@ -92,19 +75,12 @@ const onSubmit = () => {
 }
 const validateInputs = () => {
   clearInputErrors()
-  if (user.value.username == null || !/^[a-zA-Z0-9]{4,12}$/ig.test(user.value.username)) {
-    validationErros.value.username.push(t('form.errors.onlyAlphanumeric'))
-    validationErros.value.username.push(t('form.errors.onlyCharactersRange', { min: '4', max: '12' }))
-  }
-  if (isInvalidEmail(user.value.email)) {
+  if (isInvalidEmail(userRegisterParams.value.email)) {
     validationErros.value.email.push(t('form.errors.mustBeAValidEmail'))
   }
-  if (isInvalidPassword(user.value.password)) {
+  if (isInvalidPassword(userRegisterParams.value.password)) {
     validationErros.value.password.push(t('form.errors.onlyAlphanumeric'))
     validationErros.value.password.push(t('form.errors.onlyCharactersRange', { min: '8', max: '20' }))
-  }
-  if (user.value.password !== user.value.repeatPassword) {
-    validationErros.value.repeatPassword.push(t('form.errors.mustBeLikePassword'))
   }
 }
 const checkIsValid = () => {
