9d0e06095ec8e640f764c6b8d55430f3d25309e0
Andre Campos <andre.tatibano@gmail.com>
2022-08-17 21:24:03 -0300

    update storage to cookie

--- composables/storage.ts
+++ composables/storage.ts
@@ -1,10 +1,14 @@
 import { User } from '@/models/User'
 
-export const setUserOnStorage = (user: User) => localStorage.setItem('user', JSON.stringify(user))
-export const getUserOnStorage = (): User => {
-  const user = localStorage.getItem('user')
-  if (user != null) {
-    return Object.assign(new User(), JSON.parse(user)) as User
+export const setUserOnCookie = (user: User) => {
+  const userFromCookie = useCookie('user')
+  userFromCookie.value = JSON.stringify(user)
+}
+
+export const getUserFromCookie = (): User => {
+  const userFromCookie = useCookie('user')
+  if (userFromCookie != null) {
+    return Object.assign(new User(), userFromCookie.value) as User
   }
   return null
 }
